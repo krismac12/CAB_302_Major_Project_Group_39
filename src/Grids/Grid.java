05a3d53c6d6ca76592445ae198fde7390840be90
krismac12 <91189116+krismac12@users.noreply.github.com>
2022-04-07 11:33:36 +1000

    Grid Class Structure

--- src/Grids/Grid.java
+++ src/Grids/Grid.java
@@ -1,6 +1,14 @@
 package Grids;
 
+import java.awt.Point;
 public class Grid
 {
+    public boolean walkable;
+    public Point coords;
 
+    public Grid(boolean walk, Point coordinates)
+    {
+        walkable = walk;
+        coords = coordinates;
+    }
 }
