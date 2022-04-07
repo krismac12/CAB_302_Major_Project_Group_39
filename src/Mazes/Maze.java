5ea60bf39d3e941ff1d208f8ad636a31f9bcb6ec
krismac12 <91189116+krismac12@users.noreply.github.com>
2022-04-07 12:09:12 +1000

    Maze Class Structure

--- src/Mazes/Maze.java
+++ src/Mazes/Maze.java
@@ -1,6 +1,16 @@
 package Mazes;
 
+import Grids.Grid;
+
+import java.awt.*;
+import java.util.HashMap;
+
 public class Maze
 {
+    public HashMap<Point, Grid> grids;
 
+    public Maze(HashMap<Point, Grid> nodes)
+    {
+        grids = nodes;
+    }
 }
